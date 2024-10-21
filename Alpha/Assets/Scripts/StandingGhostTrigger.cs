3e5a756bb03161ca9814144c0f3aceab1e4aee27
Dongwook Kim <dwdanielkim95@gmail.com>
2024-10-21 14:10:28 -0700

    standing ghost, trigger events

---
+++ Alpha/Assets/Scripts/StandingGhostTrigger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingGhostTrigger : MonoBehaviour
+{
+    public GameObject standingGhost;
+    public GameObject ghost;
+    public GameObject lights;
+    private bool happened = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            if(!happened)
+            {
+                standingGhost.SetActive(false);
+                ghost.SetActive(true);
+                lights.SetActive(false);
+                happened = true;
+            }
+        }
+    }
+}
