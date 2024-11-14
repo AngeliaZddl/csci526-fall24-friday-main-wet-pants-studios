69137c4b7efb24d8fb10f4dea01cf5fa147a177f
Nightglow8088 <mbo@usc.edu>
2024-11-13 19:31:40 -0800

    delete useless level1 ui and optimize san bar more obvirous

--- Now You See Me/Assets/Scripts/KevinScriptFolder/Sanity/ghostSanity.cs
+++ Now You See Me/Assets/Scripts/KevinScriptFolder/Sanity/ghostSanity.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ghostSanity : MonoBehaviour
 {
@@ -8,9 +9,19 @@ public class ghostSanity : MonoBehaviour
     public PlayerSanity playerSanity;
     public KevinCameraShake kevinCameraShake;
     public GameObject jumpScareObject;
-    public GhostBack ghostBackScript; // 引用 GhostBack 脚本
+    public GameObject colorBar; // 需要更改颜色的目标对象
 
+    //public GhostBack ghostBackScript; // 引用 GhostBack 脚本
 
+    private void setSanBarToRedColor()
+    {
+        colorBar.GetComponent<Image>().color = Color.red;
+    }
+
+    private void setSanBarToGreenColor()
+    {
+        colorBar.GetComponent<Image>().color = Color.green;
+    }
 
 
     // 当带有 "player" 标签的对象停留在触发器内时调用
@@ -20,10 +31,19 @@ public class ghostSanity : MonoBehaviour
         //If player stay in the ghost's trigger, sanity lose
         if (other.CompareTag("Ghost"))
         {
-
+            setSanBarToRedColor();
             // 调用另一个脚本中的函数
             playerSanity.decreaseSanity();
 
+        }
+    }
+
+    // 当带有 "Ghost" 标签的对象离开触发器时调用
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ghost"))
+        {
+            setSanBarToGreenColor();
         }
     }
 
@@ -32,18 +52,23 @@ public class ghostSanity : MonoBehaviour
         // 检查是否是带有 "player" 标签的对象
         if (collision.gameObject.CompareTag("Ghost"))
         {
-            
+            setSanBarToRedColor();
             playerSanity.directlyLoseSanity();
             //在摇摆之前让ghost返回原位吧 如果有api要做的就在这里弄好了
-            ghostBackScript.ReturnToOriginalPosition();
+            //ghostBackScript.ReturnToOriginalPosition();
+            // 获取碰撞对象的 GhostBack 脚本，并调用 ReturnToOriginalPosition
+            GhostBack ghostBackScript = collision.gameObject.GetComponent<GhostBack>();
+            if (ghostBackScript != null)
+            {
+                ghostBackScript.ReturnToOriginalPosition();
+            }
 
 
             StartCoroutine(ShakeAndWait());
 
-
-
         }
     }
+
 
     private IEnumerator ShakeAndWait()
     {
@@ -52,12 +77,13 @@ public class ghostSanity : MonoBehaviour
         jumpScareObject.SetActive(true);
 
 
-        // 等待 3 秒
-        yield return new WaitForSeconds(3);
+        // 等待 1 秒
+        yield return new WaitForSeconds(1);
         kevinCameraShake.StopShake();
 
 
         jumpScareObject.SetActive(false);
+        setSanBarToGreenColor();
 
     }
 
